d23cb9724c2d8bb7cb682860496720f5d0a9f69e
niconielsen32 <55984156+niconielsen32@users.noreply.github.com>
2020-10-16 12:42:30 +0200

    Update setupDataLogging.sh

--- setupDataLogging.sh
+++ setupDataLogging.sh
@@ -5,10 +5,6 @@ echo Starting...
 sudo pip3 install pushbullet.py
 sudo pip3 install keyboard
 
-git clone https://github.com/niconielsen32/rimephasElectronics.git
-
-cd rimephasElectronics/dataLog
-
 echo Program Running!
 echo press "o" for output file
 echo press "q" for exit
